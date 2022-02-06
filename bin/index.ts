c723dc3e5ea3e8a5c78af0519e575174698a22f1
Cementator <46320092+Cementator@users.noreply.github.com>
2022-02-06 17:25:48 +0100

    feat: added input through command line

--- bin/index.ts
+++ bin/index.ts
@@ -16,19 +16,28 @@ class parsingText {
     textInside: string = ''
     arrayOfUrls: string[] = []
 
+
+    // Checks input for file or for input through the terminal
     checkInput() {
 
         if (process.argv.length < 3) {
-            console.log('Please input: parsetext + FILENAME');
-            process.exit(1);
+            console.log('Please input: text you want to parse fo urls');
+            process.stdin.on('data', (chunk) => {
+                this.textInside += chunk;
+                this.readInput()
+            });
+
+        }
+        if(process.argv[2]){
+            fileHandle.readFile(process.argv[2])
         }
 
     }
 
+    // main function to start reading file
     async readFile(filePath: string) {
         try {
             this.textInside = await fs.readFile(filePath, 'utf8')
-            console.log(this.textInside)
 
         } catch (e) {
             console.log(e)
@@ -36,16 +45,24 @@ class parsingText {
         }
         this.findUrl()
 
-        await this.parseUrlResponse()
+        await this.getResponse()
     }
 
+    async readInput() {
+        this.findUrl()
+
+        await this.getResponse()
+    }
+
+
+    // timer async function, needs to be awaited
     async secondsTimeout(ms: number | undefined) {
         return new Promise((resolve) => {
             setTimeout(resolve, ms)
         })
     }
 
-
+    // this functions parses text file and searches for urls inside it
     findUrl() {
 
 
@@ -65,7 +82,7 @@ class parsingText {
 
             if(stateOfOpenBracket === 1 && isBackSlashBeforeBracket === false){
                 if(character ===']'|| character ==='[' || character === ' ' || character === /\r/ || character === /\n/){
-                    let foundUrl:string[] | null = temporaryString.match(/www\.[a-z0-9A-Z]+\.[a-z0-9-A-Z]+/)
+                    let foundUrl:string[] | null = temporaryString.match(/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/i)
 
                     if(foundUrl !== null) {
                         temporaryUrls.push(foundUrl[0])
@@ -104,9 +121,10 @@ class parsingText {
             }
 
         }
-        console.log(this.arrayOfUrls)
+
     }
 
+    // encrypt found email into hexadecimal hash
     encryptEmail(email:string) {
 
         const secret = process.env.IM_SECRET
@@ -118,48 +136,106 @@ class parsingText {
         return hashValue
     }
 
-    async parseUrlResponse() {
+    // for given array of urls gets webpages data
+    async getResponse() {
         const arr: string[] = this.arrayOfUrls
 
-        for (let data of arr) {
+        if(arr.length === 0 && process.argv[2]){
+            console.log(`No web pages found in file : ${process.argv[2]}`)
+        }
+
+        for (let urlAddress of arr) {
+
             let loadTime:number = 0
             const beforeGet: number = moment.now()
-            await axios.get("https://" + data).then((response) =>{
-                console.log(response)
-                loadTime = moment().diff(beforeGet)
-                console.log(loadTime)
+
+            await axios.get("https://" + urlAddress).then((response) =>{
+
+                const afterGet:any = moment.now()
+
+                loadTime = moment(afterGet).diff(beforeGet)
+
                 if(response.status === 200){
 
-                    let url: string = data
-                    let title: string = cheerio.load(response.data)('title').text()
-                    let stringifiedData: string = response.data.toString()
-                    let email: RegExpMatchArray = stringifiedData.match(/[a-z0-9A-Z]+\.?[a-z0-9A-Z]+@[a-z0-9A-Z]+\.[a-z0-9A-Z]+/) || []
-                    let encryptedEmail:string = ''
-
-
-                    if(email[0]){
-                       encryptedEmail = this.encryptEmail(email[0])
-                    }
-
-                    let pageInformations: any = {
-                        url: url,
-                        title: title || undefined,
-                        email: encryptedEmail  || undefined
-                    }
-                    console.log(pageInformations)
+                    this.parseUrlResponse(urlAddress, response.data)
                 }
+
             },(error)=>{
-                console.log(error)
+
+                this.tryUrlAgain(urlAddress)
             })
+
             if(loadTime < 1000){
                 await this.secondsTimeout(1000 - loadTime)
             }
         }
     }
+
+    // writes stdout in terminal for given url
+    async parseUrlResponse(urlAddress: string, dataFromResponse: string | cheerio.Node | cheerio.Node[] | Buffer){
+
+        let url: string = urlAddress
+        let title: string = cheerio.load(dataFromResponse)('title').text()
+        let stringifiedData: string = dataFromResponse.toString()
+        let email: RegExpMatchArray = stringifiedData.match(/[a-z0-9A-Z]+\.?[a-z0-9A-Z]+@[a-z0-9A-Z]+\.[a-z0-9A-Z]+/) || []
+        let encryptedEmail:string = ''
+
+        if(email[0]){
+            encryptedEmail = this.encryptEmail(email[0])
+        }
+
+        let pageInformations: any = {
+            url: url,
+            title: title || undefined,
+            email: encryptedEmail  || undefined
+        }
+        this.outputResponse(pageInformations)
+
+    }
+
+    // print parsed page information in terminal
+
+    outputResponse(pageInformations: any){
+      //  if(process.argv[2]){
+            process.stdout.write(JSON.stringify(pageInformations,null, )+ "\n")
+  //      }
+
+    }
+
+    // this function is called when we cannot get status 200 from the response
+    // it makes second request after 60 seconds and if it doesn't succeed print error in stderr
+    async tryUrlAgain(urlAddress:string){
+
+        await this.secondsTimeout(60000)
+        let loadTime:number = 0
+        const beforeGet: number = moment.now()
+
+        await axios.get("https://" + urlAddress).then((response) =>{
+
+            const afterGet:any = moment.now()
+
+            loadTime = moment(afterGet).diff(beforeGet)
+
+            if(response.status === 200){
+
+                this.parseUrlResponse(urlAddress, response.data)
+            }
+
+        },(error)=>{
+            process.stderr.write(error.message+ "\n")
+
+        })
+        if(loadTime < 1000){
+            await this.secondsTimeout(1000 - loadTime)
+        }
+
+    }
+
 }
 
 const fileHandle: parsingText = new parsingText();
 
 fileHandle.checkInput()
 
-fileHandle.readFile(process.argv[2])
+
+
